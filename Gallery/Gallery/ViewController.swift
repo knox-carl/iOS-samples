f38dff1422e60285db6dc125ca9a7f1371cee2cd
Carl R. Knox <carlrknox@Carls-Mac-mini.local>
2015-01-21 11:06:52 -0700

    Gallery of Views and Image Views... More auto layout practice.

---
+++ Gallery/Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Carl R Knox on 1/21/15.
+//  Copyright (c) 2015 Carl R Knox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
